fff42444eabfd4ad341aecfa51f87577b4b3d03a
Marie Joiner <142051890+fatpounder@users.noreply.github.com>
2024-05-06 16:44:58 -0500

    Create variables.go

---
+++ variables.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var c, python, java bool
+
+func main() {
+	var i int
+	fmt.Println(i, c, python, java)
+}
+
+//var declares list of variables, in function lists the type is last
+//var can be at package or function
