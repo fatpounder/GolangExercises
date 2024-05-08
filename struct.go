f2a967ce431bd185a9e50bbfdcd7528941dc4bc2
Marie Joiner <142051890+fatpounder@users.noreply.github.com>
2024-05-07 21:08:06 -0500

    Update struct.go

--- struct.go
+++ struct.go
@@ -7,8 +7,15 @@ type Vertex struct {
 	Y int
 }
 
+//func main() {
+//	fmt.Println(Vertex{1, 2})
+//}
+
 func main() {
-	fmt.Println(Vertex{1, 2})
+	v := Vertex{1, 2}
+	v.X = 4
+	fmt.Println(v.X)
 }
 
 //a struct is a collection of fields
+//struct fields are accessed using a dot
