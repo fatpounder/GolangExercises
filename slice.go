ef78de02fc0e89592b6f24724d36f23681efdc4a
Marie Joiner <142051890+fatpounder@users.noreply.github.com>
2024-05-08 20:52:09 -0500

    Update slice.go

--- slice.go
+++ slice.go
@@ -13,3 +13,6 @@ func main() {
 //when slicing, you can omit the high or low bounds to use their defaults instead
 //default for low bound is zero and default for high bound is the length of the array
 //for the array var a [10]int, the following slices are the same: a[0:10], a[:10], a[0:], a[:]
+//the capacity of a slice is the number of elements in the referenced array, counting the first element in the slice
+//the length and capacity of a slice s can be obtained using the expressions len(s) and cap(s)
+//you can extend a slice's length by re-slicing it, provided it has sufficient capacity
