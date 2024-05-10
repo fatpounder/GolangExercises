0abf3a1add028430cd7d6b1eb117fba17bde0a68
Marie Joiner <142051890+fatpounder@users.noreply.github.com>
2024-05-09 21:41:30 -0500

    Update exercise19.go

--- exercises/exercise19.go
+++ exercises/exercise19.go
@@ -9,13 +9,24 @@ func main() {
 	x := rand.Intn(250)
 	fmt.Printf("The value of x is %v\t", x)
 
-	if x <= 100 {
+	//if x <= 100 {
+	//	fmt.Println("x is less than 100")
+	//} else if x >= 101 && x <= 200 {
+	//	fmt.Println("x is between 101 and 200")
+	//} else if x >= 201 && x <= 250 {
+	//	fmt.Println("x is between 201 and 250")
+	//} else {
+	//	fmt.Println("x is more than 250")
+	//}
+
+	switch {
+	case x <= 100:
 		fmt.Println("x is less than 100")
-	} else if x >= 101 && x <= 200 {
+	case x >= 101 && x <= 200:
 		fmt.Println("x is between 101 and 200")
-	} else if x >= 201 && x <= 250 {
+	case x >= 201 && x <= 250:
 		fmt.Println("x is between 201 and 250")
-	} else {
+	default:
 		fmt.Println("x is more than 250")
 	}
 }
