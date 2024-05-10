2caecd3a351dcfdbb2f2f509bb4a49fa9538b9e1
Marie Joiner <142051890+fatpounder@users.noreply.github.com>
2024-05-09 22:10:43 -0500

    Update range.go

--- exercises/range.go
+++ exercises/range.go
@@ -12,3 +12,5 @@ func main() {
 
 //range iterates over a slice or map
 //when ranging over a slice, two values are returned for each iteration: first the index, then the copy of the element at that index
+//can skip the index or value by assigning to _ ex:for _, value := range pow
+//if you want only the index, you can omit the second variable
