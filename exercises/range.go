32247213247e0f99b6087759f0f372c761d162fc
Marie Joiner <142051890+fatpounder@users.noreply.github.com>
2024-05-09 22:08:51 -0500

    Create range.go

---
+++ exercises/range.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+var pow = []int{1, 2, 4, 8, 16, 32, 64, 128}
+
+func main() {
+	for i, v := range pow {
+		fmt.Printf("2**%d = %d\n", i, v)
+	}
+}
+
+//range iterates over a slice or map
+//when ranging over a slice, two values are returned for each iteration: first the index, then the copy of the element at that index
