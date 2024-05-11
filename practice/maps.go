5dc5172db5542f19470e3f1a3d5b1d597aa41cea
Marie Joiner <142051890+fatpounder@users.noreply.github.com>
2024-05-10 21:37:47 -0500

    Create maps.go

---
+++ practice/maps.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+type Vertex struct {
+	Lat, Long float64
+}
+
+var m map[string]Vertex
+
+func main() {
+	m = make(map[string]Vertex)
+	m["Bell Labs"] = Vertex{
+		40.68433, -74.39967,
+	}
+	fmt.Println(m["Bell Labs"])
+}
+
+//the zero value of a map is nil. A nil map has no keys, nor can keys be added
