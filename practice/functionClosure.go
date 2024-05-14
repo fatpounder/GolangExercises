c7702e06895ce0f2b6105e46eb1d20c31463117b
Marie Joiner <142051890+fatpounder@users.noreply.github.com>
2024-05-13 21:04:21 -0500

    closure

---
+++ practice/functionClosure.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func adder() func(int) int {
+	sum := 0
+	return func(x int) int {
+		sum += x
+		return sum
+	}
+}
+
+func main() {
+	pos, neg := adder(), adder()
+	for i := 0; i < 10; i++ {
+		fmt.Println(
+			pos(i),
+			neg(-2*i),
+		)
+	}
+}
+
+//a closure is a function value that references variables from outside its body
+//the function may access and assign to the reference variables; the function is 'bound' to the variables
