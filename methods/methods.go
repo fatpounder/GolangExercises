85725ea0b513660740f8ed396dbd8eed43242c0a
Marie Joiner <142051890+fatpounder@users.noreply.github.com>
2024-05-14 21:49:09 -0500

    Create methods.go

---
+++ methods/methods.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func main() {
+	v := Vertex{3, 4}
+	fmt.Println(v.Abs())
+}
+
+//no classes in Go, but can define methods on types
+//a method is a function with a special "receiver" argument
+//the receiver appears as its own argument list between the func keyword and the method name
+//here, the Abs methods has a receiver of type Vertex named v
